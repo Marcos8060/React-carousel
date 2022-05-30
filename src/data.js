f79279ca1672893b391478ea556af416e33b98af
Marcos8060 <marcosgav80@gmail.com>
2022-05-30 14:31:45 +0300

    initial commit

---
+++ src/data.js
@@ -0,0 +1,20 @@
+const data =[
+    {
+        id: 1,
+        name: 'marcos',
+        desc: 'I am the best react developer in the world',
+        img: 'https://cdn.pixabay.com/photo/2020/02/24/09/11/man-4875665__340.jpg'
+    },
+    {
+        id: 2,
+        name: 'marcos',
+        desc: 'I am the best react developer in the world',
+        img: 'https://cdn.pixabay.com/photo/2016/06/20/22/32/mercedes-benz-1470115__340.jpg'
+    }
+    , {
+        id: 3,
+        name: 'marcos',
+        desc: 'I am the best react developer in the world',
+        img: 'https://cdn.pixabay.com/photo/2016/06/20/22/35/mercedes-benz-1470136__340.jpg'
+    }
+]
