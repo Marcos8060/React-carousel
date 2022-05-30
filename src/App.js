6ac82d0acb854c5f9610981696baa9279156afe8
Marcos8060 <marcosgav80@gmail.com>
2022-05-30 16:15:34 +0300

    installed swiper modules

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import "./App.css";
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import data from "./data";
 
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
-
+import { useState } from "react";
 
 function App() {
-  return (
+  const [items,setItems] = useState(data)
+  return ( 
     <Swiper
-      spaceBetween={20}
+      modules={[Navigation, Pagination, Scrollbar, A11y]}
+      className='slide'
+      spaceBetween={70}
       slidesPerView={3}
       navigation
       pagination={{ clickable: true }}
@@ -18,11 +23,11 @@ function App() {
       onSlideChange={() => console.log('slide change')}
       onSwiper={(swiper) => console.log(swiper)}
     >
-      <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
-      ...
+      {items.map((item) =>(
+        <SwiperSlide key={item.id}>
+          <img src={item.img} alt="" />
+        </SwiperSlide>
+      ))}
     </Swiper>
   );
 }
