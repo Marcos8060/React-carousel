a354be8f68469710f4edd212175d65cf5aecc477
Marcos8060 <marcosgav80@gmail.com>
2022-05-30 18:20:30 +0300

    finished the carousel functionality

--- src/App.js
+++ src/App.js
@@ -1,33 +1,30 @@
 import "./App.css";
-import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import data from "./data";
 
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
+import 'swiper/scss';
+import 'swiper/scss/navigation';
+import 'swiper/scss/pagination';
 import { useState } from "react";
+
 
 function App() {
   const [items,setItems] = useState(data)
-  return ( 
+  return (
     <Swiper
+      className="slide"
       modules={[Navigation, Pagination, Scrollbar, A11y]}
-      className='slide'
-      spaceBetween={70}
-      slidesPerView={3}
       navigation
-      pagination={{ clickable: true }}
-      scrollbar={{ draggable: true }}
+      spaceBetween={20}
+      slidesPerView={3}
       onSlideChange={() => console.log('slide change')}
       onSwiper={(swiper) => console.log(swiper)}
     >
-      {items.map((item) =>(
-        <SwiperSlide key={item.id}>
-          <img src={item.img} alt="" />
-        </SwiperSlide>
-      ))}
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
     </Swiper>
   );
 }
